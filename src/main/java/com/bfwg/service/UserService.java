183995689717ac3a565caae8ec36a60ac0cb6cbc
Subodh Kumar <sukumar@apptio.com>
2020-08-23 05:32:55 +0530

    first commit

---
+++ src/main/java/com/bfwg/service/UserService.java
@@ -0,0 +1,11 @@
+package com.bfwg.service;
+
+import com.bfwg.model.User;
+
+import java.util.List;
+
+public interface UserService {
+    User findById(Long id);
+    User findByUsername(String username);
+    List<User> findAll ();
+}
